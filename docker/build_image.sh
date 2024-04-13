45902002d8f13df7832315d4df895337390fb37e
Johannes Griss <jgriss8@yahoo.com>
2024-04-13 08:05:02 +0200

    Added Dockerfile and script to build image

---
+++ docker/build_image.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+function check_error {
+    STATUS_CODE=$1
+
+    if [ $STATUS_CODE != 0 ]; then
+        echo "Error: ${2}"
+        exit 1
+    fi
+}
+
+echo "+-----------------------------+"
+echo "| GREIN Proxy - Image builder |"
+echo "+-----------------------------+"
+echo ""
+
+echo -n "Tag: "
+read IMAGE_TAG
+
+# build the python image
+cd ..
+rm -rf dist
+python3 -m build
+
+check_error $? "Failed to build python package"
+
+# copy the wheel package
+cp dist/*.whl docker/
+check_error $? "Failed to copy wheel file"
+
+# add the test dataset
+cp tests/grein_proxy.db docker/grein.db
+check_error $? "Failed to copy test database"
+
+# build the docker file
+cd docker
+podman build -t quay.io/grisslab/grein_proxy:${IMAGE_TAG} .
+check_error $? "Failed to build image."
+
+echo -n "Push to quay.io? [y/N]: "
+read ANSWER
+
+if [ "$ANSWER" == "y" ];  then 
+    podman push quay.io/grisslab/grein_proxy:${IMAGE_TAG}
+fi
